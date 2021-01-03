4b44b808eceee7084f1ed944e01f46960b5e932c
Adrian Apodaca <aapodaca@gmail.com>
2021-01-03 10:07:06 -0800

    first pass at thumb component

--- components/main_page/post_thumb.jsx
+++ components/main_page/post_thumb.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import parse from 'html-react-parser';
+import {Link} from 'react-router-dom';
 
 export const PostThumb = ({post}) => {
 
@@ -6,7 +8,8 @@ export const PostThumb = ({post}) => {
         <span>
             <img className="thumbnail-image" src={post.jetpack_featured_media_url}></img>
             <h3 className="post-title">{post.title.rendered}</h3>
-            <p>{post.excerpt.rendered}</p>
+            <p>{parse(post.excerpt.rendered)}</p>
+
         </span>
 
     )

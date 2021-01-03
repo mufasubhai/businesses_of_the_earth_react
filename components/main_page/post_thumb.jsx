d9ff0197984bc0f81bbc8d3b2a6689f6ab9ca644
Adrian Apodaca <aapodaca@gmail.com>
2021-01-03 11:39:15 -0800

    continue thumbnail work - pause work on shortened texT

--- components/main_page/post_thumb.jsx
+++ components/main_page/post_thumb.jsx
@@ -3,14 +3,42 @@ import parse from 'html-react-parser';
 import {Link} from 'react-router-dom';
 
 export const PostThumb = ({post}) => {
+    const cloneDeep = require('lodash.clonedeep')
+    const trimmedText  = textObject => {
+        let newObject = cloneDeep(textObject);
+        console.log(newObject)
+
+       
+        let text = Array.from(newObject[0].props.children)
+            
+        // let newText = text.split("").splice(0, 200)
+
+        // console.log(text);
+        // console.log(typeof text);
+        // console.log(typeof Array.from(text))
+
+            // if (newText.length >= 200) {
+            //     newText = newText.push("...")
+            // }
+
+            // let newSentence = ""
+            // newText.forEach(char => {
+            //     newSentence += char;
+            // })
+
+            // newObject[0].props.children = newSentence;
+            
+            // return [newObject, ""]
+            }
+        
+        
 
     return (
-        <span>
+        <Link>
             <img className="thumbnail-image" src={post.jetpack_featured_media_url}></img>
             <h3 className="post-title">{post.title.rendered}</h3>
-            <p>{parse(post.excerpt.rendered)}</p>
-
-        </span>
+            <span>{parse(post.excerpt.rendered)}</span>
+        </Link>
 
     )
 }

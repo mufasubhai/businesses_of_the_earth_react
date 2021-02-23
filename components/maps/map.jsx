36f5b6608a5e820ef7ccdc6e7286a483540f46b6
Adrian Apodaca <aapodaca@gmail.com>
2021-02-23 08:09:41 -0800

    add map wrapper

--- components/maps/map.jsx
+++ components/maps/map.jsx
@@ -9,22 +9,25 @@ import GoogleMapReact from 'google-map-react'
 const GoogleMap = props => {
 let defaultProps = {
     center: {
-      lat: 59.95,
-      lng: 30.33
+      lat: 34.0522,
+      lng: -118.2437
     },
     zoom: 11
   };
 
 
     return (
+        <div className="map-wrapper ">
+
         <div id="map">
            <GoogleMapReact
           bootstrapURLKeys={{ key: MAPI}}
           defaultCenter={defaultProps.center}
           defaultZoom={defaultProps.zoom}
-        >
+          >
           
         </GoogleMapReact>
+        </div>
         </div>
     );
 }
